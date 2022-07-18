b50340942ab03c29e7c0e6814f2176bc1d4427e5
Felix Klauke <felix@klauke-enterprises.com>
2022-07-18 11:34:07 +0200

    Make delete immutable

--- src/main/java/de/d3adspace/lucy/Delete.java
+++ src/main/java/de/d3adspace/lucy/Delete.java
@@ -3,45 +3,42 @@ package de.d3adspace.lucy;
 import java.util.Objects;
 
 public final class Delete {
-  private String table;
-  private Condition where;
+  private final String table;
+  private final Condition where;
 
-  public Delete(String table) {
+  private Delete(String table, Condition where) {
     this.table = table;
+    this.where = where;
   }
 
   public static Delete from(String table) {
-    Objects.requireNonNull(table);
-    return new Delete(table);
+    return new Delete(table, null);
   }
 
   public Delete table(String table) {
-    this.table = table;
-    return this;
+    return new Delete(table, where);
   }
 
   public Delete where(Condition where) {
-    this.where = where;
-    return this;
+    return new Delete(table, where);
+
   }
 
   public Delete orWhere(Condition where) {
-    this.where = this.where == null ? where : this.where.orWhere(where);
-    return this;
+    return new Delete(table, this.where == null ? where : this.where.orWhere(where));
   }
 
   public Delete andWhere(Condition where) {
-    this.where = this.where == null ? where : this.where.andWhere(where);
-    return this;
+    return new Delete(table, this.where == null ? where : this.where.andWhere(where));
   }
 
   public Delete xorWhere(Condition where) {
-    this.where = this.where == null ? where : this.where.xorWhere(where);
-    return this;
+    return new Delete(table, this.where == null ? where : this.where.xorWhere(where));
   }
 
   @Override
   public String toString() {
+    Objects.requireNonNull(table);
     return "DELETE FROM " + table + (where == null ? "" : " WHERE " + where);
   }
 }
