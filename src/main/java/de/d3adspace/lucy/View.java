c972b91517ebeeaa3dc2286f21fa75de8b84e871
Felix Klauke <felix@klauke-enterprises.com>
2022-07-18 11:08:28 +0200

    Add view update and drop support

--- src/main/java/de/d3adspace/lucy/View.java
+++ src/main/java/de/d3adspace/lucy/View.java
@@ -2,24 +2,56 @@ package de.d3adspace.lucy;
 
 public final class View {
   private final String name;
+  private boolean replaceIfExists;
   private Select select;
 
-  private View(String name, Select select) {
+  private View(String name, boolean replaceIfExists, Select select) {
     this.name = name;
+    this.replaceIfExists = replaceIfExists;
     this.select = select;
+  }
+
+  public Drop drop() {
+    return new Drop(name);
+  }
+
+  public static Drop drop(String name) {
+    return new Drop(name);
   }
 
   public static View named(String name) {
-    return new View(name, null);
+    return new View(name, false, null);
   }
 
-  public View as(Select select) {
+  public View createAs(Select select) {
     this.select = select;
+    return this;
+  }
+
+  public View replaceIfExists() {
+    return replaceIfExists(true);
+  }
+
+  public View replaceIfExists(boolean replaceIfExists) {
+    this.replaceIfExists = replaceIfExists;
     return this;
   }
 
   @Override
   public String toString() {
-    return "CREATE VIEW " + name + " AS " + select;
+    return "CREATE VIEW " + (replaceIfExists ? "OR REPLACE " : "") + name + " AS " + select;
+  }
+
+  public static final class Drop {
+    private final String name;
+
+    public Drop(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return "DROP VIEW " + name;
+    }
   }
 }
--- src/test/java/de/d3adspace/lucy/ViewTest.java
+++ src/test/java/de/d3adspace/lucy/ViewTest.java
@@ -7,14 +7,31 @@ import org.junit.jupiter.api.Test;
 class ViewTest {
   @Test
   void test() {
-    View view = View.named("test").as(Select.from("test"));;
+    var view = View.named("test").createAs(Select.from("test"));;
     assertEquals("CREATE VIEW test AS SELECT * FROM test", view.toString());
   }
 
   @Test
-  void test2() {
-    View view = View.named("test");
-    view.as(Select.from("test").where(Condition.equal("id", 1)));
+  void testWithWhere() {
+    var view = View.named("test").createAs(Select.from("test").where(Condition.equal("id", 1)));
     assertEquals("CREATE VIEW test AS SELECT * FROM test WHERE id = 1", view.toString());
+  }
+
+  @Test
+  void testWithWhereAndReplaceIfExists() {
+    var view = View.named("test").replaceIfExists().createAs(Select.from("test").where(Condition.equal("id", 1)));
+    assertEquals("CREATE VIEW OR REPLACE test AS SELECT * FROM test WHERE id = 1", view.toString());
+  }
+
+  @Test
+  void testDrop() {
+    var view = View.drop("test");
+    assertEquals("DROP VIEW test", view.toString());
+  }
+  
+  @Test
+  void testInnerDrop() {
+    var view = View.named("test").drop();
+    assertEquals("DROP VIEW test", view.toString());
   }
 }
